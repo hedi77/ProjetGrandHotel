aebde5a520ad39a7f9ce4f31127f54c8e115de8e
1 <2>
2018-01-11 13:29:21 +0100

    go

---
+++ GrandHotel/GrandHotel/Entites.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandHotel
+{
+    //public class Client
+    //{
+    //    public int Id { get; set; }
+    //    public string Civilite { get; set; }
+    //    public string Nom { get; set; }
+    //    public string Prenom { get; set; }
+    //    public bool CarteFidelite { get; set; }
+    //    public string Societe { get; set; }
+    //}
+}
---
+++ GrandHotel/GrandHotel/GrandHotelContexte.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandHotel
+{
+    public class GrandHotelContext : DbContext
+    {
+
+        //public DbSet<Client> Clients { get; set; }
+        //public static IList<string> GetPaysFournisseurs()
+        //{
+        //    var list = new List<string>();
+        //    var cmd = new SqlCommand();
+        //    cmd.CommandText = @"select * from Client";
+
+        //    using (var cnx = new SqlConnection(Settings1.Default.GrandHotelConnexion))
+        //    {
+
+        //        cmd.Connection = cnx;
+        //        cnx.Open();
+
+        //        using (SqlDataReader reader = cmd.ExecuteReader())
+        //        {
+
+        //            while (reader.Read())
+        //            {
+        //                list.Add((string)reader["Nom"]);
+        //            }
+        //        }
+        //    }
+        //    return list;
+        //}
+        /*
+        public IList<Client> GetClients()
+        {
+            Clients.Load();
+
+            return Clients.Local;
+        }*/
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+
+    }
+}
